41274646aaf1671cb9c7b20c8452f054ef930e3b
Subhankar Raj <subhankar.rj@gmail.com>
2021-02-11 23:22:05 +0530

    Use express

--- functions/index.js
+++ functions/index.js
@@ -3,32 +3,36 @@ const admin = require('firebase-admin')
 
 admin.initializeApp()
 
+const express = require('express')
+const app = express()
 
-exports.getScreams = functions.https.onRequest((req, res) => {
+app.get('/screams', (req, res) => {
     admin
         .firestore()
         .collection('screams')
+        .orderBy('createdAt', 'desc')
         .get()
         .then(data => {
             let screams = [];
             data.forEach(doc => {
-                screams.push(doc.data());
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
             });
             return res.json(screams);
         })
         .catch(err => console.error(err));
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
+app.post('/scream', (req, res) => {
 
-    if(req.method !== 'POST') {
-        return res.status(400).json({ error: 'Method not allowed'});
-    }
-    
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
     admin
@@ -43,3 +47,5 @@ exports.createScream = functions.https.onRequest((req, res) => {
             console.error(err)
         });
 });
+
+exports.api = functions.https.onRequest(app);
