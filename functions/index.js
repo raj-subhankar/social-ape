2684f6e523c59baf238cc70c8399cf53fb80c1c3
raj-subhankar <subhankar.rj@gmail.com>
2021-02-25 20:42:31 +0530

    Get and create comments

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,21 @@
 const functions = require("firebase-functions");
-
 const app = require('express')();
-
 const FBAuth = require('./util/fbauth');
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
+const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams');
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
-
 
 //Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
-app.post('/user/image', FBAuth, uploadImage);
-app.post('/user', FBAuth, addUserDetails);
-app.get('/user', FBAuth, getAuthenticatedUser);
+app.get('/scream/:screamId', getScream)
+app.get('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 //Users routes
 app.post('/signup', signup);
 app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
